ebf5873e945aaf066a6ed49f4f66c01fe6b4ad53
Philipp Rieber <p.rieber@gmail.com>
2016-06-11 23:20:23 +0200

    extract local git config to include file; adapt OSX settings; improve installation process

--- osx_install.sh
+++ osx_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo Install all AppStore Apps at first!
+echo May install XCode from AppStore?
 # no solution to automate AppStore installs
 read -p "Press any key to continue... " -n1 -s
 echo  '\n'
@@ -9,12 +9,10 @@ xcode-select --install
 
 source ./.osx
 
-# Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 ./brew.sh
 
 # SSH Keygen
 # $ ssh-keygen -t rsa -C 'your@email.com'
 #  [ follow prompts ]
 # $ pbcopy < ~/.ssh/id_rsa.pub
+# $ cp .gitconfig.local.example ~/.gitconfig.local
